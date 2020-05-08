f5552c1493950952d19ebc65db8a57980015d44e
xinggevip <1511844263@qq.com>
2020-05-08 18:59:18 +0800

    java环境准备

---
+++ java/regular01/src/main/java/com/example/Regular01Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Regular01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Regular01Application.class, args);
+    }
+
+}
